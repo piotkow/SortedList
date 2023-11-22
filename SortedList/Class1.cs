7461ffdf85e2b925bd76fa43a499e94fd1912138
piotkow <piotkow133@student.polsl.pl>
2023-11-22 16:04:41 +0100

    done

---
+++ SortedList/Class1.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+public class CustomSortedCollection<TKey, TValue> : IEnumerable<KeyValuePair<TKey, TValue>>
+
+{
+    private List<KeyValuePair<TKey, TValue>> items = new List<KeyValuePair<TKey, TValue>>();
+
+    public void AddElement(TKey key, TValue value)
+    {
+        items.Add(new KeyValuePair<TKey, TValue>(key, value));
+        items = items.OrderBy(item => item.Key).ToList();
+    }
+
+    public bool RemoveElement(TKey key)
+    {
+        int indexToRemove = items.FindIndex(item => EqualityComparer<TKey>.Default.Equals(item.Key, key));
+
+        if (indexToRemove >= 0)
+        {
+            items.RemoveAt(indexToRemove);
+            return true;
+        }
+
+        return false;
+    }
+
+    public void DisplayElements()
+    {
+        Console.WriteLine("Elements in the collection:");
+        foreach (var item in items)
+        {
+            Console.WriteLine($"Key: {item.Key}, Value: {item.Value}");
+        }
+    }
+
+    public IEnumerator<KeyValuePair<TKey, TValue>> GetEnumerator()
+    {
+        return items.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+}
---
+++ UI/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        var customCollection = new CustomSortedCollection<int, string>();
+
+        customCollection.AddElement(3, "Three");
+        customCollection.AddElement(1, "One");
+        customCollection.AddElement(2, "Two");
+        customCollection.AddElement(5, "Five");
+        customCollection.AddElement(8, "Eight");
+        customCollection.AddElement(9, "Nine");
+
+        customCollection.DisplayElements();
+
+        Console.WriteLine("Greater than 4 with LINQ: ");
+        var fileredItems = customCollection.Where(x => x.Key > 4);
+        foreach (var item in fileredItems)
+        {
+            Console.WriteLine(item);
+        }
+
+        bool removed = customCollection.RemoveElement(1);
+        Console.WriteLine($"\nRemoved element with key 1: {removed}");
+
+        customCollection.DisplayElements();
+    }
+}
